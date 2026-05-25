566203a49740fade78f678458769c5a4d50485a4
Cheetah's Creations <sleek.cheetah@gmail.com>
2026-05-24 22:36:23 -0400

    Create __init__.py

---
+++ src/chts/__init__.py
@@ -0,0 +1,6 @@
+# src/chts/__init__.py
+from chts.thermal import radiative_flux, conductive_flux
+from chts.teg import calculate_zt, teg_stage_efficiency
+from chts.plasma import saha_ionization_fraction
+
+__version__ = "0.1.0"
